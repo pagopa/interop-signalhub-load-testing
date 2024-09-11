66c321beb30c9a117c95d1f94e5dea2d16977202
Paolo Manca <paolo.manca@pagopa.it>
2024-09-11 15:33:14 +0200

    adding params

--- src/pull.gatling.ts
+++ src/pull.gatling.ts
@@ -1,16 +1,23 @@
 import { simulation, scenario, exec, rampUsers } from "@gatling.io/core";
 import { http } from "@gatling.io/http";
-import { baseUrlPull, duration, eserviceId, rampUsersProfile, users, voucher } from "./lib/setup";
+import {
+  baseUrl,
+  duration,
+  eserviceId,
+  givenUsersDistributedInADuration,
+  users,
+  voucher
+} from "./lib/setup";
 
 export default simulation((setUp) => {
   console.log(
-    `### STARTING LOAD TEST against ${baseUrlPull} with ${users} users for ${duration} seconds ###`
+    `### STARTING LOAD TEST against ${baseUrl} with ${users} users for ${duration} seconds ###`
   );
   const signalId = 0;
   const size = 100;
 
   // Define an HTTP protocol
-  const httpProtocol = http.baseUrl(baseUrlPull);
+  const httpProtocol = http.baseUrl(baseUrl);
 
   // Write the scenario PULL
   const useCasePullSignals = exec(
@@ -23,5 +30,7 @@ export default simulation((setUp) => {
   const pullingSignalsScenario = scenario("Pull").exec(useCasePullSignals);
 
   // Define the injection profile
-  setUp(pullingSignalsScenario.injectOpen(rampUsersProfile)).protocols(httpProtocol);
+  setUp(pullingSignalsScenario.injectOpen(givenUsersDistributedInADuration)).protocols(
+    httpProtocol
+  );
 });
--- src/push.gatling.ts
+++ src/push.gatling.ts
@@ -1,15 +1,23 @@
 import { simulation, scenario, exec, rampUsers, StringBody } from "@gatling.io/core";
 import { http } from "@gatling.io/http";
 import { createSignal, generateRandomSignalId, toJson } from "./lib";
-import { baseUrlPush, duration, eserviceId, rampUsersProfile, users, voucher } from "./lib/setup";
+import {
+  baseUrl,
+  duration,
+  incrementUsers,
+  givenUsersDistributedInADuration,
+  users,
+  voucher,
+  eserviceId
+} from "./lib/setup";
 
 export default simulation((setUp) => {
   console.log(
-    `### STARTING LOAD TEST against ${baseUrlPush} with ${users} users for ${duration} seconds ###`
+    `### STARTING LOAD TEST against ${baseUrl} with ${users} users for ${duration} seconds ###`
   );
 
   // Define an HTTP protocol
-  const httpProtocol = http.baseUrl(baseUrlPush);
+  const httpProtocol = http.baseUrl(baseUrl);
 
   const buildSignalPayload = (session: any) => {
     const signalId = session.get("signalId");
@@ -31,5 +39,7 @@ export default simulation((setUp) => {
     .exec(useCasePushingSignals);
 
   // Define the injection profile
-  setUp(pushingSignalsScenario.injectOpen(rampUsersProfile)).protocols(httpProtocol);
+  setUp(pushingSignalsScenario.injectOpen(givenUsersDistributedInADuration)).protocols(
+    httpProtocol
+  );
 });

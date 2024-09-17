9397c576f3d07ccdf3a4495692f1a16f0c1ef556
Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>
2024-09-17 18:24:05 +0200

    Only Comments to help - small updates (#6)

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -10,14 +10,18 @@ export const baseUrl = getEnvironmentVariable("BASE_URL", "http://localhost:3001
 export const users = parseInt(getParameter("users", "1"));
 export const duration = parseInt(getParameter("duration", "5"));
 
-export const startingUsers = parseInt(getParameter("startingUsers", "500"));
-export const incrementUsers = parseFloat(getParameter("incrementUsers", "100"));
+export const startingUsers = parseInt(getParameter("startingUsers", "100"));
+export const incrementUsers = parseFloat(getParameter("incrementUsers", "25"));
 export const eachLevelLasting = parseInt(getParameter("eachLevelLasting", "5"));
 export const incrementTimes = parseInt(getParameter("incrementTimes", "5"));
-// tot req increments = (100 * 5 * 5) = 5000
-// tot req starting = 10 * 5 * 5 = 250
-// tot req 5250
-// export const separatedByRampsLasting = parseInt(getParameter("separatedByRampsLasting", "5"));
+
+// level 0, (starting 100 * 5 sec) + (increment 25 * 5 sec) = 625 users
+// level 1, (starting 100 * 5 sec) + (increment 25 * 5 sec) = 625 users
+// level 2, (starting 100 * 5 sec) + (increment 25 * 5 sec) = 625 users
+// level 3, (starting 100 * 5 sec) + (increment 25 * 5 sec) = 625 users
+// level 4. (starting 100 * 5 sec) + (increment 25 * 5 sec) = 625 users
+// level 5, (starting 100 * 5 sec) + (increment 25 * 5 sec) = 625 users
+// tot req 625 * 6 = 3750
 
 export const voucher = getParameter("voucher", "");
 export const eserviceId = getParameter("eserviceId", "31b4e4e6-855d-42fa-9705-28bc7f8545ff");
@@ -26,12 +30,7 @@ export const profile = getParameter("profile", "ramping"); // increment
 
 export const givenUsersDistributedInADuration = rampUsers(users).during(duration);
 
-// generate an open workload injection profile
-// with levels of 10, 15, 20, 25 and 30 arriving users per second
-// each level lasting 10 seconds
-// separated by linear ramps lasting 10 seconds
 export const injectedUsersFromStartingRateToTargetRate = incrementUsersPerSec(incrementUsers)
   .times(incrementTimes)
   .eachLevelLasting(eachLevelLasting)
-  // .separatedByRampsLasting(separatedByRampsLasting)
   .startingFrom(startingUsers); // Double
